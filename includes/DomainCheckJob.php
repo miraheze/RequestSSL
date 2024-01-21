ad432aa7be5b83b71b219493de43fa8a864d040f
Alex <alex@blueselene.com>
2024-01-21 18:39:55 +0100

    fix

--- includes/DomainCheckJob.php
+++ includes/DomainCheckJob.php
@@ -2,18 +2,20 @@
 
 namespace Miraheze\RequestSSL;
 
+use GenericParameterJob;
 use Job;
 use MediaWiki\HookContainer\HookContainer;
 use Miraheze\RequestSSL\RequestSSLManager;
 use User;
 
-class DomainCheckJob extends Job {
+class DomainCheckJob extends Job implements GenericParameterJob {
 	public function __construct( array $params ) {
 		parent::__construct( 'DomainCheckJob', $params );
 	}
 
 	public function run() {
-		$requestSslManager = new RequestSSLManager()->fromID( $this->params['requestID'] );
+		$requestSslManager = new RequestSSLManager();
+		$requestSslManager->fromID( $this->params['requestID'] );
 		$isPointed = false;
 		HookContainer::run( 'RequestSSLDomainCheck', [&$requestSslManager, &$isPointed] );
 		if ( $isPointed ) {
