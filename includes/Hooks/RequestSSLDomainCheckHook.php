2608738500471aa9bc532f62120388284d02120b
Alex <alex@blueselene.com>
2024-01-27 20:05:39 +0100

    fix

--- includes/Hooks/RequestSSLDomainCheckHook.php
+++ includes/Hooks/RequestSSLDomainCheckHook.php
@@ -4,8 +4,8 @@ namespace Miraheze\RequestSSL\Hooks;
 
 interface RequestSSLDomainCheckHook {
 	/**
-	 * @param RequestSSLManager &$requestSslManager
-	 * @param bool isPointed
+	 * @param Miraheze\RequestSSL\RequestSSLManager &$requestSslManager
+	 * @param bool &$isPointed
 	 * @return void
 	 */
 	public function onRequestSSLDomainCheck( &$requestSslManager, &$isPointed ): void;
