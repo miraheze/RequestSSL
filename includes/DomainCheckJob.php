ef0bd57454932b25ae2ca64471c271807714fe4a
Alex <alex@blueselene.com>
2024-03-02 16:51:07 +0100

    fix run function

--- includes/DomainCheckJob.php
+++ includes/DomainCheckJob.php
@@ -10,8 +10,9 @@ class DomainCheckJob extends Job implements GenericParameterJob {
 	public function __construct( array $params ) {
 		parent:__construct( 'DomainCheckJob', $params );
 	}
-	public function run()
-	$requestSslManager = MediaWikiServices::getInstance()->get( 'RequestSSLManager' );
-	$requestSslManager->fromID( $this->params['requestID'] );
-	// Do something useful
+	public function run() {
+		$requestSslManager = MediaWikiServices::getInstance()->get( 'RequestSSLManager' );
+		$requestSslManager->fromID( $this->params['requestID'] );
+		// Do something useful
+	}
 }
