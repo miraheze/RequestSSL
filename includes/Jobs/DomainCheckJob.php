407a0be38be3fd6c58ec994f4252dbe863f1343f
Alex <alex@blueselene.com>
2024-03-10 15:21:33 +0100

    Inject services into RequestSSLManager

--- includes/Jobs/DomainCheckJob.php
+++ includes/Jobs/DomainCheckJob.php
@@ -2,37 +2,62 @@
 
 namespace Miraheze\RequestSSL\Jobs;
 
+use Config;
+use ConfigFactory;
 use GenericParameterJob;
 use Job;
-use MediaWiki\MediaWikiServices;
+use Miraheze\RequestSSL\RequestSSLManager;
 use User;
 
 class DomainCheckJob extends Job implements GenericParameterJob {
-	public function __construct( array $params ) {
+
+	/** @var Config */
+	private $config;
+
+	/** @var int */
+	private $requestID;
+
+	/** @var RequestSSLManager */
+	private $requestSslManager;
+
+	/**
+	 * @param array $params
+	 * @param ConfigFactory $configFactory
+	 * @param RequestSSLManager $requestSslManager
+	*/
+	public function __construct(
+		array $params,
+		ConfigFactory $configFactory,
+		RequestSSLManager $requestSslManager
+	) {
 		parent::__construct( 'DomainCheckJob', $params );
+		$this->requestID = $params['requestID'];
+		$this->config = $configFactory->makeConfig( 'RequestSSL' );
+		$this->requestSslManager = $requestSslManager;
 	}
+
+	/**
+	 * @return bool
+	 */
 	public function run() {
-		$mwServices = MediaWikiServices::getInstance();
-		$requestSslManager = $mwServices->get( 'RequestSSLManager' );
-		$requestSslManager->fromID( $this->params['requestID'] );
-		$customDomain = parse_url( $requestSslManager->getCustomDomain(), PHP_URL_HOST );
+		$this->requestSslManager->fromID( $this->requestID );
+		$customDomain = parse_url( $this->requestSslManager->getCustomDomain(), PHP_URL_HOST );
 		if ( !$customDomain ) {
 			// Custom domain does not have a hostname, bail out.
 			// TODO: Log an exception.
 			return true;
 		}
-		$config = $mwServices->getConfigFactory()->makeConfig('RequestSSL');
-		$cname = $config->get( 'RequestSSLDomainCheckCNAME' );
+		$cname = $this->config->get( 'RequestSSLDomainCheckCNAME' );
 		// TODO: Support rDNS and NS checks
 		// CNAME check
 		$dnsCNAMEData = dns_get_record( $customDomain, DNS_CNAME );
 		if ( !$dnsCNAMEData ) {
-			$requestSslManager->addComment( 'RequestSSL could not determine whether or not this domain is pointed: DNS returned no data during CNAME check.', User::newSystemUser( 'RequestSSL Extension' ) );
+			$this->requestSslManager->addComment( 'RequestSSL could not determine whether or not this domain is pointed: DNS returned no data during CNAME check.', User::newSystemUser( 'RequestSSL Extension' ) );
 		} else {
 			if ( $dnsCNAMEData[0]['type'] === 'CNAME' && $dnsCNAMEData[0]['target'] === $cname ) {
-				$requestSslManager->addComment( 'Domain is pointed via CNAME.', User::newSystemUser( 'RequestSSL Extension' ) );
+				$this->requestSslManager->addComment( 'Domain is pointed via CNAME.', User::newSystemUser( 'RequestSSL Extension' ) );
 			} else {
-				$requestSslManager->addComment( 'Domain is not pointed via CNAME. It is possible it is pointed via other means.', User::newSystemUser( 'RequestSSL Extension' ) );
+				$this->requestSslManager->addComment( 'Domain is not pointed via CNAME. It is possible it is pointed via other means.', User::newSystemUser( 'RequestSSL Extension' ) );
 			}
 		}
 		return true;
